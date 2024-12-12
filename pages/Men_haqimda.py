d574f136023185abb35008d4c858696f01174d4d
Umidjon <147255061+Umidjonjuraqulov@users.noreply.github.com>
2024-12-12 19:35:20 +0300

    Update Men_haqimda.py

--- pages/Men_haqimda.py
+++ pages/Men_haqimda.py
@@ -1,1 +1,5 @@
+import streamlit as st
 
+st.title("Raximova Husnora")
+st.subheader("403-Guruh")
+st.image("https://avatars.mds.yandex.net/i?id=c0fffc291582b417765269c0935423824832eb0c-9700546-images-thumbs&n=13")
